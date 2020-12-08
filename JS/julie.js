19349d6c0736757d767c1732bbdda41c19661553
JulieBrusberg <julie@brusberg.dk>
2020-12-08 13:26:52 +0100

    h

--- JS/julie.js
+++ JS/julie.js
@@ -10,4 +10,10 @@ function scrollperso() {
 function scrollfri() {
     var elem = document.getElementById("fri");
     elem.scrollIntoView();
-} 
+}
+var frivillige = Array("CAFEFRIVLLIG", "AKTIVITETSFRIVILLIG", "TELEFONRÅDGIVNING", "ÅENTSIND UNGEGRUPPE", "PEER TO PEER", "SKIBHUSKABYSSEN"),
+    selectEl = document.getElementById('frivillige');
+
+for (var i = 0; i < frivillige.length; i++) {
+    selectEl.options.add(new Option(frivillige[i], frivillige[i]));
+}
