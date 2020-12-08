c58c152c150b4c6aa5d9befa0fa95760a539eaa8
JulieBrusberg <julie@brusberg.dk>
2020-12-08 08:40:56 +0100

    nn

---
+++ JS/julie.js
@@ -0,0 +1,13 @@
+
+function scrollomos() {
+    var elem = document.getElementById("omos");
+    elem.scrollIntoView();
+}
+function scrollperso() {
+    var elem = document.getElementById("perso");
+    elem.scrollIntoView();
+}
+function scrollfri() {
+    var elem = document.getElementById("fri");
+    elem.scrollIntoView();
+} 
