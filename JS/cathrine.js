cd98ff1a468415e0574868d24d52fb6e3b580ec6
cathrinexschmidt <70138870+cathrinexschmidt@users.noreply.github.com>
2020-12-10 10:28:44 +0100

    HTML & CSS

--- JS/cathrine.js
+++ JS/cathrine.js
@@ -1,12 +1,3 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+    document.getElementById("demo").innerHTML = "";
 }
-
-document.getElementById('foo').addEventListener('click', function (e) {
-
-    var img = document.createElement('img');
-
-    img.setAttribute('src', 'http://blog.stackoverflow.com/wp-content/uploads/stackoverflow-logo-300.png');
-
-    e.target.appendChild(img);
-});
