57a5c7d12d25cbf3b1ebcd72ad29e427b578dd1c
cathrinexschmidt <70138870+cathrinexschmidt@users.noreply.github.com>
2020-12-10 11:24:47 +0100

    dch

--- JS/cathrine.js
+++ JS/cathrine.js
@@ -1,3 +1,16 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "";
+// Alert //
+alert("OBS: HUSK AT LÆSE OM DE NYE COVID-19 RETNINGSLIJNER!")
+
+// Scroll function //
+function scrollVoresTilbud() {
+    document.getElementById("voresTilbud").scrollIntoView();
+}
+
+function scrollVoresKalender() {
+    document.getElementById("voresKalender").scrollIntoView();
+}
+
+// Hide & show event //
+function klikStoppestedet() {
+    document.getElementById("klikStoppestedet").innerHTML = "";
 }
