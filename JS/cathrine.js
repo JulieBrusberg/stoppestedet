0b6cbed2b5fada06c26a48296f0466c6db21d86d
cathrinexschmidt <70138870+cathrinexschmidt@users.noreply.github.com>
2020-12-11 15:10:22 +0100

    Hide & Show

--- JS/cathrine.js
+++ JS/cathrine.js
@@ -35,3 +35,69 @@ function klikStoppestedet2() {
         }
     }
 }
+
+function klikStoppestedet3() {
+    var u = document.getElementById("klikPeer"); {
+        if (u.style.display === "none") {
+            u.style.display = "block";
+        }
+        else {
+            u.style.display = "none"
+        }
+    }
+}
+
+function klikStoppestedet4() {
+    var u = document.getElementById("klikSam"); {
+        if (u.style.display === "none") {
+            u.style.display = "block";
+        }
+        else {
+            u.style.display = "none"
+        }
+    }
+}
+
+function klikStoppestedet5() {
+    var u = document.getElementById("klikPG"); {
+        if (u.style.display === "none") {
+            u.style.display = "block";
+        }
+        else {
+            u.style.display = "none"
+        }
+    }
+}
+
+function klikStoppestedet6() {
+    var u = document.getElementById("klikSK"); {
+        if (u.style.display === "none") {
+            u.style.display = "block";
+        }
+        else {
+            u.style.display = "none"
+        }
+    }
+}
+
+function klikStoppestedet7() {
+    var u = document.getElementById("klikTlfR"); {
+        if (u.style.display === "none") {
+            u.style.display = "block";
+        }
+        else {
+            u.style.display = "none"
+        }
+    }
+}
+
+function klikStoppestedet8() {
+    var u = document.getElementById("klikBS"); {
+        if (u.style.display === "none") {
+            u.style.display = "block";
+        }
+        else {
+            u.style.display = "none"
+        }
+    }
+}
