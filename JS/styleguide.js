5c4661bfb7ec13d7f95bda0a22b338e7ead5e6dc
JulieBrusberg <julie@brusberg.dk>
2020-12-13 00:17:53 +0100

    styleguide færdig

---
+++ JS/styleguide.js
@@ -0,0 +1,34 @@
+function scrolltofarver() {
+    document.getElementById("farver").scrollIntoView();
+}
+
+
+function scrolltoformer() {
+    document.getElementById("former").scrollIntoView();
+}
+
+
+function scrolltomood() {
+    document.getElementById("mood").scrollIntoView();
+}
+
+
+function scrolltoskrift() {
+    document.getElementById("skrift").scrollIntoView();
+}
+
+
+function scrolltoknap() {
+    document.getElementById("knap").scrollIntoView();
+}
+
+
+
+function scrolltoilu() {
+    document.getElementById("ilu").scrollIntoView();
+}
+
+
+function scrolltoikon() {
+    document.getElementById("ikon").scrollIntoView();
+}
